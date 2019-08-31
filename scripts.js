860acbbb60f06e4aa9748b41e03aa570f5d7aac2
Robby Renz <nxgrenz@gmail.com>
2019-08-31 10:01:52 -0500

    basic functionality of program now working

--- scripts.js
+++ scripts.js
@@ -1,8 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
+
+	const keys = document.querySelectorAll(".key")
+	keys.forEach(key => key.addEventListener("transitionend", removeTransition))
+
   document.addEventListener("keydown", e => {
+    whichKey(e)
+  })
+
+  function whichKey(e) {
+  	let key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+  	if (key == null)
+  		return
     let audio = document.querySelector("audio")
     audio.currentTime = 0
     audio.play()
-    console.log(e)
-  })
+  	key.classList.add("playing")
+  	console.log(key)
+  }
+
+  function removeTransition(e) {
+  	this.classList.remove("playing")
+  }
+
+
 })
